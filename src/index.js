4dfb3a0266e8e148c727d055c811d72c6a4a6d0d
wavymav <mavjbarnes@gmail.com>
2022-08-21 22:21:42 -0700

    v0.1.1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as animation from "./shared/animation";
+import * as global from "./shared/global";
+import * as icons from "./shared/icons";
+import * as styles from "./shared/styles";
+
+export { styles, global, animation, icons };
+
+export * from "./Avatar";
+export * from "./Badge";
+export * from "./Button";
+export * from "./Icon";
+export * from "./Link";
